15cbde64fe9c9bdcb7092f66f09c935cc8985da7
史海 <shihai@pv25.com>
2016-06-30 11:40:16 +0800

    init app

---
+++ khttp.c
@@ -0,0 +1,190 @@
+
+/* This file was generated automatically by Zephir do not modify it! */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <php.h>
+
+#if PHP_VERSION_ID < 50500
+#include <locale.h>
+#endif
+
+#include "php_ext.h"
+#include "khttp.h"
+
+#include <ext/standard/info.h>
+
+#include <Zend/zend_operators.h>
+#include <Zend/zend_exceptions.h>
+#include <Zend/zend_interfaces.h>
+
+#include "kernel/globals.h"
+#include "kernel/main.h"
+#include "kernel/fcall.h"
+#include "kernel/memory.h"
+
+
+
+zend_class_entry *khttp_request_ce;
+
+ZEND_DECLARE_MODULE_GLOBALS(khttp)
+
+PHP_INI_BEGIN()
+	
+PHP_INI_END()
+
+static PHP_MINIT_FUNCTION(khttp)
+{
+#if PHP_VERSION_ID < 50500
+	char* old_lc_all = setlocale(LC_ALL, NULL);
+	if (old_lc_all) {
+		size_t len = strlen(old_lc_all);
+		char *tmp  = calloc(len+1, 1);
+		if (UNEXPECTED(!tmp)) {
+			return FAILURE;
+		}
+
+		memcpy(tmp, old_lc_all, len);
+		old_lc_all = tmp;
+	}
+
+	setlocale(LC_ALL, "C");
+#endif
+	REGISTER_INI_ENTRIES();
+	ZEPHIR_INIT(Khttp_Request);
+
+#if PHP_VERSION_ID < 50500
+	setlocale(LC_ALL, old_lc_all);
+	free(old_lc_all);
+#endif
+	return SUCCESS;
+}
+
+#ifndef ZEPHIR_RELEASE
+static PHP_MSHUTDOWN_FUNCTION(khttp)
+{
+
+	zephir_deinitialize_memory(TSRMLS_C);
+	UNREGISTER_INI_ENTRIES();
+	return SUCCESS;
+}
+#endif
+
+/**
+ * Initialize globals on each request or each thread started
+ */
+static void php_zephir_init_globals(zend_khttp_globals *khttp_globals TSRMLS_DC)
+{
+	khttp_globals->initialized = 0;
+
+	/* Memory options */
+	khttp_globals->active_memory = NULL;
+
+	/* Virtual Symbol Tables */
+	khttp_globals->active_symbol_table = NULL;
+
+	/* Cache Enabled */
+	khttp_globals->cache_enabled = 1;
+
+	/* Recursive Lock */
+	khttp_globals->recursive_lock = 0;
+
+	/* Static cache */
+	memset(khttp_globals->scache, '\0', sizeof(zephir_fcall_cache_entry*) * ZEPHIR_MAX_CACHE_SLOTS);
+
+
+}
+
+/**
+ * Initialize globals only on each thread started
+ */
+static void php_zephir_init_module_globals(zend_khttp_globals *khttp_globals TSRMLS_DC)
+{
+
+}
+
+static PHP_RINIT_FUNCTION(khttp)
+{
+
+	zend_khttp_globals *khttp_globals_ptr = ZEPHIR_VGLOBAL;
+
+	php_zephir_init_globals(khttp_globals_ptr TSRMLS_CC);
+	//zephir_init_interned_strings(TSRMLS_C);
+
+	zephir_initialize_memory(khttp_globals_ptr TSRMLS_CC);
+
+
+	return SUCCESS;
+}
+
+static PHP_RSHUTDOWN_FUNCTION(khttp)
+{
+
+	
+
+	zephir_deinitialize_memory(TSRMLS_C);
+	return SUCCESS;
+}
+
+static PHP_MINFO_FUNCTION(khttp)
+{
+	php_info_print_box_start(0);
+	php_printf("%s", PHP_KHTTP_DESCRIPTION);
+	php_info_print_box_end();
+
+	php_info_print_table_start();
+	php_info_print_table_header(2, PHP_KHTTP_NAME, "enabled");
+	php_info_print_table_row(2, "Author", PHP_KHTTP_AUTHOR);
+	php_info_print_table_row(2, "Version", PHP_KHTTP_VERSION);
+	php_info_print_table_row(2, "Build Date", __DATE__ " " __TIME__ );
+	php_info_print_table_row(2, "Powered by Zephir", "Version " PHP_KHTTP_ZEPVERSION);
+	php_info_print_table_end();
+
+	DISPLAY_INI_ENTRIES();
+}
+
+static PHP_GINIT_FUNCTION(khttp)
+{
+	php_zephir_init_globals(khttp_globals TSRMLS_CC);
+	php_zephir_init_module_globals(khttp_globals TSRMLS_CC);
+}
+
+static PHP_GSHUTDOWN_FUNCTION(khttp)
+{
+
+}
+
+
+zend_function_entry php_khttp_functions[] = {
+ZEND_FE_END
+
+};
+
+zend_module_entry khttp_module_entry = {
+	STANDARD_MODULE_HEADER_EX,
+	NULL,
+	NULL,
+	PHP_KHTTP_EXTNAME,
+	php_khttp_functions,
+	PHP_MINIT(khttp),
+#ifndef ZEPHIR_RELEASE
+	PHP_MSHUTDOWN(khttp),
+#else
+	NULL,
+#endif
+	PHP_RINIT(khttp),
+	PHP_RSHUTDOWN(khttp),
+	PHP_MINFO(khttp),
+	PHP_KHTTP_VERSION,
+	ZEND_MODULE_GLOBALS(khttp),
+	PHP_GINIT(khttp),
+	PHP_GSHUTDOWN(khttp),
+	NULL,
+	STANDARD_MODULE_PROPERTIES_EX
+};
+
+#ifdef COMPILE_DL_KHTTP
+ZEND_GET_MODULE(khttp)
+#endif
---
+++ php_khttp.h
@@ -0,0 +1,76 @@
+
+#ifndef PHP_KHTTP_H
+#define PHP_KHTTP_H 1
+
+#ifdef PHP_WIN32
+#define ZEPHIR_RELEASE 1
+#endif
+
+#include "kernel/globals.h"
+
+#define PHP_KHTTP_NAME        "khttp"
+#define PHP_KHTTP_VERSION     "0.1.1"
+#define PHP_KHTTP_EXTNAME     "khttp"
+#define PHP_KHTTP_AUTHOR      "Sgenmi"
+#define PHP_KHTTP_ZEPVERSION  "0.9.3a-dev-5c0fb106a6"
+#define PHP_KHTTP_DESCRIPTION "QQ:150560159"
+
+
+
+ZEND_BEGIN_MODULE_GLOBALS(khttp)
+
+	int initialized;
+
+	/* Memory */
+	zephir_memory_entry *start_memory; /**< The first preallocated frame */
+	zephir_memory_entry *end_memory; /**< The last preallocate frame */
+	zephir_memory_entry *active_memory; /**< The current memory frame */
+
+	/* Virtual Symbol Tables */
+	zephir_symbol_table *active_symbol_table;
+
+	/** Function cache */
+	HashTable *fcache;
+
+	zephir_fcall_cache_entry *scache[ZEPHIR_MAX_CACHE_SLOTS];
+
+	/* Cache enabled */
+	unsigned int cache_enabled;
+
+	/* Max recursion control */
+	unsigned int recursive_lock;
+
+	/* Global constants */
+	zval *global_true;
+	zval *global_false;
+	zval *global_null;
+	
+ZEND_END_MODULE_GLOBALS(khttp)
+
+#ifdef ZTS
+#include "TSRM.h"
+#endif
+
+ZEND_EXTERN_MODULE_GLOBALS(khttp)
+
+#ifdef ZTS
+	#define ZEPHIR_GLOBAL(v) TSRMG(khttp_globals_id, zend_khttp_globals *, v)
+#else
+	#define ZEPHIR_GLOBAL(v) (khttp_globals.v)
+#endif
+
+#ifdef ZTS
+	#define ZEPHIR_VGLOBAL ((zend_khttp_globals *) (*((void ***) tsrm_ls))[TSRM_UNSHUFFLE_RSRC_ID(khttp_globals_id)])
+#else
+	#define ZEPHIR_VGLOBAL &(khttp_globals)
+#endif
+
+#define ZEPHIR_API ZEND_API
+
+#define zephir_globals_def khttp_globals
+#define zend_zephir_globals_def zend_khttp_globals
+
+extern zend_module_entry khttp_module_entry;
+#define phpext_khttp_ptr &khttp_module_entry
+
+#endif
